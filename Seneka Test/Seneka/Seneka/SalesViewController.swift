4383d029dc9d710e39651e778b990c7cbf42b637
Santiago Mendoza <s.mendoza11@uniandes.edu.co>
2019-02-10 19:46:58 -0500

    Botón para ir a la pantalla de ventas

--- Seneka Test/Seneka/Seneka/SalesViewController.swift
+++ Seneka Test/Seneka/Seneka/SalesViewController.swift
@@ -13,6 +13,8 @@ class LogInController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print ("View has loaded")
     }
     
     
