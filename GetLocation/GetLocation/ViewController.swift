03071565dc2a03d8c0cd90002b5e12b587855b23
Paula Gómez <alejandra.villalobos9@gmail.com>
2019-02-10 19:23:35 -0500

    Agregado para obtener coordenadas

--- GetLocation/GetLocation/ViewController.swift
+++ GetLocation/GetLocation/ViewController.swift
@@ -8,31 +8,40 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController ,  CLLocationManagerDelegate{
     
-    var locationManager = CLLocationManager()
-    
-
+    //Map
     @IBOutlet weak var map: MKMapView!
+    let manager = CLLocationManager()
     
-    override func viewDidLoad() {
+    func  locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpan.init(latitudeDelta: 0.002,longitudeDelta: 0.002)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region: MKCoordinateRegion=MKCoordinateRegion.init(center: myLocation, span: span)
+        map.setRegion(region, animated: true)
+        
+        self.map.showsUserLocation=true
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.locationManager.requestWhenInUseAuthorization()
         
-        if CLLocationManager.locationServicesEnabled(){
-        
-            locationManager.delegate=self
-        locationManager.desiredAccuracy=kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
-        }
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
-        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    }
+}
+
 
 
