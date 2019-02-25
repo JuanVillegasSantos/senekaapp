2ace058e5cdb29e7cff4bfeb447843e9ed2ef410
Santiago Mendoza <s.mendoza11@uniandes.edu.co>
2019-02-25 15:19:36 -0500

    Added Profile Assets
    Added Profile Assets

--- Seneka Test/Seneka/Seneka/ProfileViewController.swift
+++ Seneka Test/Seneka/Seneka/ProfileViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
+    var arrayOfData = [UIImage]()
+    var arrayOfDataIDs = [String]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arrayOfData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        let imageView = cell.viewWithTag(1) as! UIImageView
+        imageView.image = arrayOfData[indexPath.row]
+        
+        return cell
+    }
         
     @IBAction func FromProfileToEmerButton(_ sender: Any) {
         print("Button pressed")
@@ -19,6 +35,12 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        arrayOfData += [UIImage(named: "ProfileLineAsset.png")!, UIImage(named: "PersonalInfoLabelAsset.png")!, UIImage(named: "AgeAsset.png")!, UIImage(named: "GenderAsset.png")!, UIImage(named: "NationalityInfoAsset.png")!, UIImage(named: "IDNumberAsset.png")!, UIImage(named: "PassportNumberAsset.png")!, UIImage(named: "MedicalInfoAsset.png")!, UIImage(named: "BloodTypeAsset.png")!, UIImage(named: "WeightAsset.png")!, UIImage(named: "WhiteAsset.png")!]
+        
+        //arrayOfData += [UIImage(named: "ProfileLineAsset.png")!, UIImage(named: "PersonalInfoAsset.png")!, UIImage(named: "NationalityInfoAsset.png")!, UIImage(named: "MedicalInfoAsset.png")!]
+        
+        arrayOfDataIDs = ["Store1","Store2"]
         
         print("Profile View has loaded")
         
